fbff0dc1d0e4e1bd56903649982c7c3e44a1b34c
Nameless <50556449+Mikami91@users.noreply.github.com>
2020-05-11 16:45:12 -0400

    Updates

---
+++ precache-manifest.af844604adbb8f73d4da2a5acb5f2f74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f86622c77685f149e44d",
+    "url": "/Kroft-FrontEnd/static/css/main.9653c399.chunk.css"
+  },
+  {
+    "revision": "f86622c77685f149e44d",
+    "url": "/Kroft-FrontEnd/static/js/main.a876e080.chunk.js"
+  },
+  {
+    "revision": "feb151ab0423f96045cd",
+    "url": "/Kroft-FrontEnd/static/js/runtime~main.b123471c.js"
+  },
+  {
+    "revision": "828f81c2cd0efa20a6d4",
+    "url": "/Kroft-FrontEnd/static/js/2.12cc00e9.chunk.js"
+  },
+  {
+    "revision": "3360c6fbba37f7d74760298e06312c55",
+    "url": "/Kroft-FrontEnd/static/media/table.3360c6fb.png"
+  },
+  {
+    "revision": "051998c008840156f5237fa0084cf9ed",
+    "url": "/Kroft-FrontEnd/static/media/user.051998c0.png"
+  },
+  {
+    "revision": "47a4c434ad75eed62262d6725211d5f8",
+    "url": "/Kroft-FrontEnd/static/media/environment.47a4c434.png"
+  },
+  {
+    "revision": "9047be420cd32058e5ccc0a66c81bccd",
+    "url": "/Kroft-FrontEnd/static/media/product.9047be42.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kroft-FrontEnd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f378075283c90232f3eaada33b52438",
+    "url": "/Kroft-FrontEnd/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Kroft-FrontEnd/precache-manifest.af5236ccbd289be801cf5ae1bafc6196.js"
+  "/Kroft-FrontEnd/precache-manifest.af844604adbb8f73d4da2a5acb5f2f74.js"
 );
 
 workbox.clientsClaim();
