471f9fc95a67c0ab81f77bb07ffaef6fc424ae42
Nameless <50556449+Mikami91@users.noreply.github.com>
2020-05-13 12:34:56 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Kroft-FrontEnd/precache-manifest.e5caaa2cd48f50a7918765b918768bd7.js"
+  "/Kroft-FrontEnd/precache-manifest.d32e887a1044fafa30feba70589d3bd1.js"
 );
 
 workbox.clientsClaim();
