364653b8ed5f5b58c4105a7a0d0f774b0d408def
Nameless <50556449+Mikami91@users.noreply.github.com>
2020-06-26 12:16:55 -0400

    Updates

---
+++ precache-manifest.5eed0330e18c0b263aa7d8da5f591f71.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a97d664f11945cb31cc",
+    "url": "/Kroft-FrontEnd/static/css/main.0e5a5145.chunk.css"
+  },
+  {
+    "revision": "6a97d664f11945cb31cc",
+    "url": "/Kroft-FrontEnd/static/js/main.5a387e39.chunk.js"
+  },
+  {
+    "revision": "feb151ab0423f96045cd",
+    "url": "/Kroft-FrontEnd/static/js/runtime~main.b123471c.js"
+  },
+  {
+    "revision": "15aeb31eb2867c8fdfdd",
+    "url": "/Kroft-FrontEnd/static/js/2.e321b5a8.chunk.js"
+  },
+  {
+    "revision": "3360c6fbba37f7d74760298e06312c55",
+    "url": "/Kroft-FrontEnd/static/media/table.3360c6fb.png"
+  },
+  {
+    "revision": "9047be420cd32058e5ccc0a66c81bccd",
+    "url": "/Kroft-FrontEnd/static/media/product.9047be42.png"
+  },
+  {
+    "revision": "051998c008840156f5237fa0084cf9ed",
+    "url": "/Kroft-FrontEnd/static/media/user.051998c0.png"
+  },
+  {
+    "revision": "18aa68692e5630e7e5daaf28a84ba39b",
+    "url": "/Kroft-FrontEnd/static/media/productbackground.18aa6869.jpg"
+  },
+  {
+    "revision": "c843d8ab1788c30172ce48b2089101e9",
+    "url": "/Kroft-FrontEnd/static/media/kroft-vertical.c843d8ab.svg"
+  },
+  {
+    "revision": "47a4c434ad75eed62262d6725211d5f8",
+    "url": "/Kroft-FrontEnd/static/media/environment.47a4c434.png"
+  },
+  {
+    "revision": "cd2a6ce50c21e066d3f2522c57d07d84",
+    "url": "/Kroft-FrontEnd/static/media/kroft-horizontal.cd2a6ce5.svg"
+  },
+  {
+    "revision": "51f62c479775b8eb2927e4773d2a6de2",
+    "url": "/Kroft-FrontEnd/static/media/food-and-restaurant (7).51f62c47.svg"
+  },
+  {
+    "revision": "b6e81422ccf733158a8674317b59cef0",
+    "url": "/Kroft-FrontEnd/static/media/food-and-restaurant (2).b6e81422.svg"
+  },
+  {
+    "revision": "43c39685646943c1218a5a1bbb81da36",
+    "url": "/Kroft-FrontEnd/static/media/food-and-restaurant (4).43c39685.svg"
+  },
+  {
+    "revision": "5a830c5a41de0a585e53c5edf2043b79",
+    "url": "/Kroft-FrontEnd/static/media/food-and-restaurant1.5a830c5a.svg"
+  },
+  {
+    "revision": "4f61683e49786db180a1bc36603d914e",
+    "url": "/Kroft-FrontEnd/static/media/cake-piece.4f61683e.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kroft-FrontEnd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7186927f7689b613de53ab2eec63655",
+    "url": "/Kroft-FrontEnd/static/media/abstract-blur-bubble-clean-268819.d7186927.jpg"
+  },
+  {
+    "revision": "94fb39e79abe5c3aecfb2202d9e14e34",
+    "url": "/Kroft-FrontEnd/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Kroft-FrontEnd/precache-manifest.d72a129cf8b8e843d188d24a9e3bbcd5.js"
+  "/Kroft-FrontEnd/precache-manifest.5eed0330e18c0b263aa7d8da5f591f71.js"
 );
 
 workbox.clientsClaim();
