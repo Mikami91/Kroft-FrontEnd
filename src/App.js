6ecc077bd8046c6d02a48698ff321c09b5f21578
Mikami91 <kaz7sense@hotmail.com>
2021-04-16 23:20:54 -0400

    isLogged for PIN fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 // Dependencies
 import React, { Fragment } from "react";
 // Routing
-import {
-  MemoryRouter as Router,
-
-  Route, Switch
-} from "react-router-dom";
+import { MemoryRouter as Router, Route, Switch } from "react-router-dom";
 // Styles
 import "./App.css";
 // Pages
@@ -17,22 +13,22 @@ function App() {
   return (
     <Fragment>
       {/* <Router> */}
-        <Switch>
-          {routes.map((prop, key) => {
-            if (prop.path !== null) {
-              return (
-                <Route
-                  exact
-                  path={prop.path}
-                  component={() => <prop.component />}
-                  // component={prop.component}
-                  key={key}
-                />
-              );
-            }
-          })}
-          <Route component={NotFound} />
-        </Switch>
+      <Switch>
+        {routes.map((prop, key) => {
+          if (prop.path !== null) {
+            return (
+              <Route
+                exact
+                path={prop.path}
+                component={() => <prop.component />}
+                // component={prop.component}
+                key={key}
+              />
+            );
+          }
+        })}
+        <Route component={NotFound} />
+      </Switch>
       {/* </Router> */}
     </Fragment>
   );
